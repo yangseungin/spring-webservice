a3b476a1e239b56160d5870d27cdb0dd25e8463f
seungin yang <rhfpdk92@naver.com>
2019-05-29 15:14:45 +0900

    PostsRepository 인터페이스 생성

---
+++ src/main/java/com/giantdwarf/webservice/domain/post/PostsRepository.java
@@ -0,0 +1,10 @@
+package com.giantdwarf.webservice.domain.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+	//보통 Mybatis에서 Dao라고 불리는 DB Layer 접근자//
+	//JPA에서는 Repository라고 부르며 인터페이스로 생성함.
+	//단순히 인터페이스 생성 후 JPaRepository<Entity클래스, PK타입>을 상속하면 기본적인 CRUD 메소드가 자동생성 됨.
+	//@Repository를 추가할 필요도 없음.
+}
